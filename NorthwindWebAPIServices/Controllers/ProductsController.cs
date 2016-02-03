cc98adf51bb90781b5d698f9610280a4bb4ffaab
Roman Arekeev <roman_arekeev@epam.com>
2016-02-03 11:54:43 +0400

    Added ProductsController.

---
+++ NorthwindWebAPIServices/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿namespace NorthwindWebAPIServices.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+    using NorthwindWebAPIServices.Models;
+
+    public class ProductsController : ApiController
+    {
+        private static readonly ProductsRepository Products = new ProductsRepository();
+
+        public IEnumerable<Product> Get()
+        {
+            return Products.GetAll();
+        }
+    }
+}
