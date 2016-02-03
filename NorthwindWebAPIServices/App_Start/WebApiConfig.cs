d8cc2f66ee5422b9f4d4b26386a177c55a2f7d1c
Roman Arekeev <roman_arekeev@epam.com>
2016-02-03 11:49:32 +0400

    Fix some style violations.

--- NorthwindWebAPIServices/App_Start/WebApiConfig.cs
+++ NorthwindWebAPIServices/App_Start/WebApiConfig.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
-
+﻿
 namespace NorthwindWebAPIServices
 {
+    using System.Web.Http;
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -17,8 +15,7 @@ namespace NorthwindWebAPIServices
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+                defaults: new { id = RouteParameter.Optional });
         }
     }
 }
