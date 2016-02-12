340b8ea5373c38b81a7b6d53c6d92c3d8fb18045
Roman Arekeev <roman_arekeev@epam.com>
2016-02-12 15:51:24 +0400

    Added Tests project.

---
+++ Tests/ProductControllerTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Tests
+{
+    using System.Web.Http;
+    using System.Web.Http.Results;
+    using Moq;
+    using NorthwindWebAPIServices.Controllers;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ProductControllerTests
+    {
+        [Test]
+        public void GetTest()
+        {
+        }
+    }
+}
